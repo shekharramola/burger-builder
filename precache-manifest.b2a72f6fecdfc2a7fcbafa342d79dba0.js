36852130dfbeaede53508b8b740d5370077acd45
shekhar <shekharramola2012@gmail.com>
2019-12-24 13:19:49 +0530

    Updates

---
+++ precache-manifest.b2a72f6fecdfc2a7fcbafa342d79dba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3553f2e5f4029624d350a01b91e683b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1096b73362f4156653a7",
+    "url": "/static/css/main.ec1246bf.chunk.css"
+  },
+  {
+    "revision": "ff62468f3b39e6fc88b7",
+    "url": "/static/js/2.6baf8192.chunk.js"
+  },
+  {
+    "revision": "1096b73362f4156653a7",
+    "url": "/static/js/main.bf561baf.chunk.js"
+  },
+  {
+    "revision": "da437af8f63d180392c4",
+    "url": "/static/js/runtime-main.dea84fda.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
